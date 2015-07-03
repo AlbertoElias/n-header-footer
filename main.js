71fb61a722332cd9816eb90fec197e833d66b6af
Paul I Wilson <paul.i.wilson@ft.com>
2015-07-03 16:56:32 +0100

    remove js stuff for login

--- main.js
+++ main.js
@@ -4,13 +4,11 @@
 var subNav = require('./src/js/sub-nav');
 var search = require('./src/js/search');
 var notified = require('./src/js/notified');
-var account = require('./src/js/account');
 
 function init(flags) {
 	subNav.init(flags);
 	search.init(flags);
 	notified.init(flags);
-	account.init(flags);
 }
 
 module.exports.init = init;
