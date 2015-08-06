ce07472d599210860354ea7f6b28bed98b4c9526
Arjun Gadhia <arjungadhia@gmail.com>
2015-08-06 11:09:33 +0100

    Remove red dot from myFT

--- main.js
+++ main.js
@@ -3,12 +3,10 @@
 
 var subNav = require('./src/js/sub-nav');
 var search = require('./src/js/search');
-var notified = require('./src/js/notified');
 
 function init(flags) {
 	subNav.init(flags);
 	search.init(flags);
-	notified.init(flags);
 }
 
 module.exports.init = init;
