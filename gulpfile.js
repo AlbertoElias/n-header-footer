9c5f087c9a5e58f33f440ec23fd67be53aad52a9
Viktor Charypar <charypar@gmail.com>
2015-08-18 14:54:58 +0100

    Move account related links to nav on mobile

--- gulpfile.js
+++ gulpfile.js
@@ -11,20 +11,23 @@ var topLevelSections = ['world', 'companies', 'markets', 'opinion', 'work & care
 
 gulp.task('build', function () {
 	return gulp.src('./templates/partials/navigation/**/*-template.html')
-		.pipe(data(function () {
-			return {
-				all: navigation.items().map(function (navItem) {
-					navItem.isTopLevel = topLevelSections.indexOf(navItem.name.toLowerCase()) > -1;
-					return navItem;
-				}),
-				nav: navigation.items().filter(function (navItem) {
-					return topLevelSections.indexOf(navItem.name.toLowerCase()) > -1;
-				})
-			};
-		}))
-		.pipe(handlebars())
-		.pipe(rename(function (path) {
-			path.basename = path.basename.replace('-template', '');
-		}))
-		.pipe(gulp.dest('./templates/partials/navigation'));
+	.pipe(data(function () {
+		var data = {
+			all: navigation.items().map(function (navItem) {
+				navItem.isTopLevel = topLevelSections.indexOf(navItem.name.toLowerCase()) > -1;
+				return navItem;
+			}),
+			nav: navigation.items().filter(function (navItem) {
+				return topLevelSections.indexOf(navItem.name.toLowerCase()) > -1;
+			})
+		};
+
+		return data;
+	}))
+	.pipe(handlebars({}, { batch: ['./templates/partials'] }))
+	.on('error', function(e) { console.log("Error compiling handlebars", e.message)})
+	.pipe(rename(function (path) {
+		path.basename = path.basename.replace('-template', '');
+	}))
+	.pipe(gulp.dest('./templates/partials/navigation'))
 });
