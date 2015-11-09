67fc91bcedae20ea68ebb8f0c4c7974f7c062c88
andygout <andy.gout@ft.com>
2015-11-09 14:14:33 +0000

    Remove: Unhandled error event

--- src/js/prevent-scroll.js
+++ src/js/prevent-scroll.js
@@ -1,7 +1,7 @@
 const init = () => {
-	document.querySelector('input[name=sub-nav]').addEventListener('change', event => {
+	document.querySelector('input[name=sub-nav]').addEventListener('change', () => {
 		document.documentElement.classList.toggle('next-navigation-is-open');
 	});	
-}
+};
 
-export default { init: init }
+export default { init: init };
