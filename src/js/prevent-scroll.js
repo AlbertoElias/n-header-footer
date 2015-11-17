6ec4c05f318c7c2115d4715f7834c323c4a83953
andygout <andy.gout@ft.com>
2015-11-17 17:00:06 +0000

    Fix: Header area scroll for mobile devices

--- src/js/prevent-scroll.js
+++ src/js/prevent-scroll.js
@@ -1,6 +1,7 @@
 const init = () => {
 	document.querySelector('input[name=sub-nav]').addEventListener('change', () => {
 		document.documentElement.classList.toggle('next-navigation-is-open');
+		document.getElementsByTagName('body')[0].classList.toggle('next-navigation-is-open');
 	});	
 };
 
