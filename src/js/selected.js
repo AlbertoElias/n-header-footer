92ad8f23ebd4be58422e680d8a40b27c02662da1
andygout <andy.gout@ft.com>
2015-11-24 11:16:12 +0000

    Amend: Selected script to make IE-friendly

--- src/js/selected.js
+++ src/js/selected.js
@@ -3,9 +3,9 @@ import Delegate from 'ftdomdelegate';
 const init = () => {
 	const delegate = new Delegate(document.querySelector('.js-header'));
 	delegate.on('click', '.js-select', (ev, select) => {
-		[...document.querySelectorAll('.js-select')].forEach(
+		Array.prototype.slice.call(document.querySelectorAll('.js-select')).forEach(
 			selectEl => selectEl.classList.remove('selected')
-		);
+		)
 		select.classList.add('selected');
 	});
 };
