442c2bf619f6df2281f6bf35e628dd1d242c88a6
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-12-11 10:56:03 +0000

    adding edition switcher

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ import search from './src/js/search';
 import preventScroll from './src/js/prevent-scroll';
 import toggle from './src/js/toggle';
 import selected from './src/js/selected';
-import editionToggler from './src/js/edition-toggler';
+import editionSwitcher from './src/js/edition-switcher';
 
 const init = flags => {
 	search.init(flags);
 	preventScroll.init();
 	toggle.init(flags);
 	selected.init(flags);
-	editionToggler.init();
+	editionSwitcher.init(document.querySelector('.edition-switcher'));
 };
 
 export default {
